4d17abf66b26052ea63a9383c92408f5ee2211cf
andrpavl <puvlo@yahoo.com>
2025-02-28 10:57:24 +0100

    Routes for the meals added

--- api/src/index.js
+++ api/src/index.js
@@ -11,14 +11,70 @@ app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// You can delete this route once you add your own routes
-apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
+// Getting future meals
+apiRouter.get("/future-meals", async (req, res) => {
+	try {
+		const upcomingMeals = await knex.raw(
+			"SELECT * FROM meal WHERE `when` > NOW()"
+		);
+
+		upcomingMeals[0].length ?
+			res.json(upcomingMeals[0])
+		:	res.status(404).json("There are no any events in the future.");
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error." });
+	}
+});
+
+//Getting past meals
+apiRouter.get("/past-meals", async (req, res) => {
+	try {
+		const pastMeals = await knex.raw("SELECT * FROM meal WHERE `when` < NOW()");
+
+		pastMeals[0].length ?
+			res.json(pastMeals[0])
+		:	res.status(404).json("There are no any past events.");
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error." });
+	}
+});
+
+//Getting all meals
+apiRouter.get("/all-meals", async (req, res) => {
+	try {
+		const allMeals = await knex.raw("SELECT * FROM meal ORDER BY id");
+		allMeals[0].length ?
+			res.json(allMeals[0])
+		:	res.status(404).json("Meals not found.");
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error." });
+	}
+});
+
+//Getting first meal
+apiRouter.get("/first-meal", async (req, res) => {
+	try {
+		const firstMeal = await knex.raw("SELECT * FROM meal ORDER BY id LIMIT 1");
+		firstMeal[0].length ?
+			res.json(firstMeal[0])
+		:	res.status(404).json("Meals not found.");
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error." });
+	}
+});
+
+//Getting last meal
+apiRouter.get("/last-meal", async (req, res) => {
+	try {
+		const lastMeal = await knex.raw(
+			"SELECT * FROM meal ORDER BY id DESC LIMIT 1"
+		);
+		lastMeal[0].length ?
+			res.json(lastMeal[0])
+		:	res.status(404).json("Meals not found.");
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error." });
+	}
 });
 
 // This nested router example can also be replaced with your own sub-router
@@ -27,5 +83,5 @@ apiRouter.use("/nested", nestedRouter);
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log(`API listening on port ${process.env.PORT}`);
+	console.log(`API listening on port ${process.env.PORT}`);
 });
